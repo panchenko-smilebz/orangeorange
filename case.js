dccff6b122a1b61f46afe33bf4e646e58b85b2eb
Vitaliy Panchenko <panchenkovitalij@gmail.com>
2024-12-23 18:30:14 +0200

    exclude all animations except hero sections on screens 991px and below

--- case.js
+++ case.js
@@ -28,270 +28,280 @@ window.addEventListener('load', function (event) {
   init();
 });
 
-// CASE HERO SECTION
-gsap.to('.case-hero_image-wrapper', {
-  scrollTrigger: {
-    trigger: '.section_case-hero',
-    start: 'top top',
-    end: 'bottom top',
-    scrub: true,
-  },
-  y: '4rem',
-});
-
-// REQUESTS SECTION
-document.querySelectorAll('.section_requests').forEach((section) => {
-  const requestsTl = gsap.timeline({
+if (window.innerWidth > 991) {
+  // CASE HERO SECTION
+  gsap.to('.case-hero_image-wrapper', {
     scrollTrigger: {
-      trigger: section,
-      start: `top ${window.innerWidth > 991 ? '15%' : '90%'}`,
-      markers: false,
+      trigger: '.section_case-hero',
+      start: 'top top',
+      end: 'bottom top',
+      scrub: true,
     },
-    ease: 'power3.inOut',
+    y: '4rem',
   });
 
-  requestsTl
-    .from(section.querySelector('.requests_section-heading'), {
-      y: '100%',
-      duration: 1,
-    })
-    .from(
-      section.querySelector('.requests_gradient'),
-      {
-        x: '100%',
+  // REQUESTS SECTION
+  document.querySelectorAll('.section_requests').forEach((section) => {
+    const requestsTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: section,
+        start: `top ${window.innerWidth > 991 ? '15%' : '90%'}`,
+        markers: false,
+      },
+      ease: 'power3.inOut',
+    });
+
+    requestsTl
+      .from(section.querySelector('.requests_section-heading'), {
         y: '100%',
-        opacity: 0,
-        duration: 0.8,
-      },
-      0.4,
-    )
-    .from(
-      section.querySelector('.requests_desc-heading'),
-      {
-        y: '3rem',
-        opacity: 0,
         duration: 1,
-      },
-      0.4,
-    )
-    .from(
-      section.querySelector('.requests_desc'),
-      {
-        y: '2rem',
-        opacity: 0,
-        duration: 0.8,
-      },
-      0.8,
-    );
-});
-
-// CASE TIMELINE SECTION
-const projectTimelineSection = document.querySelector(
-  '.section_project-timeline',
-);
-
-const caseTimelineTl = gsap.timeline({
-  scrollTrigger: {
-    trigger: projectTimelineSection,
-    start: `top ${scrollTriggerStart}`,
-    markers: false,
-  },
-  ease: 'power3.inOut',
-});
-
-caseTimelineTl
-  .from(projectTimelineSection.querySelector('.cases_section-heading'), {
-    y: '100%',
-    duration: 1,
-  })
-  .from('.project-timeline_image-wrapper', {
-    y: '3rem',
-    opacity: 0,
-    duration: 0.8,
+      })
+      .from(
+        section.querySelector('.requests_gradient'),
+        {
+          x: '100%',
+          y: '100%',
+          opacity: 0,
+          duration: 0.8,
+        },
+        0.4,
+      )
+      .from(
+        section.querySelector('.requests_desc-heading'),
+        {
+          y: '3rem',
+          opacity: 0,
+          duration: 1,
+        },
+        0.4,
+      )
+      .from(
+        section.querySelector('.requests_desc'),
+        {
+          y: '2rem',
+          opacity: 0,
+          duration: 0.8,
+        },
+        0.8,
+      );
   });
 
-// PROCESS SECTION
-document.querySelectorAll('.section_process').forEach((section) => {
-  const requestsTl = gsap.timeline({
+  // CASE TIMELINE SECTION
+  const projectTimelineSection = document.querySelector(
+    '.section_project-timeline',
+  );
+
+  const caseTimelineTl = gsap.timeline({
     scrollTrigger: {
-      trigger: section,
+      trigger: projectTimelineSection,
       start: `top ${scrollTriggerStart}`,
       markers: false,
     },
     ease: 'power3.inOut',
   });
 
-  requestsTl
-    .from(section.querySelector('.cases_section-heading'), {
+  caseTimelineTl
+    .from(projectTimelineSection.querySelector('.cases_section-heading'), {
+      y: '100%',
+      duration: 1,
+    })
+    .from('.project-timeline_image-wrapper', {
+      y: '3rem',
+      opacity: 0,
+      duration: 0.8,
+    });
+
+  // PROCESS SECTION
+  document.querySelectorAll('.section_process').forEach((section) => {
+    const requestsTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: section,
+        start: `top ${scrollTriggerStart}`,
+        markers: false,
+      },
+      ease: 'power3.inOut',
+    });
+
+    requestsTl
+      .from(section.querySelector('.cases_section-heading'), {
+        y: '100%',
+        duration: 1,
+      })
+      .from(
+        section.querySelector('.process_gradient'),
+        {
+          x: '100%',
+          y: '100%',
+          opacity: 0,
+          duration: 0.8,
+        },
+        0.4,
+      )
+      .from(
+        section.querySelector('.process_stage-heading'),
+        {
+          y: '3rem',
+          opacity: 0,
+          duration: 1,
+        },
+        0.4,
+      )
+      .from(
+        section.querySelectorAll('.process-item'),
+        {
+          y: '2rem',
+          opacity: 0,
+          stagger: 0.2,
+          duration: 0.8,
+        },
+        0.8,
+      )
+      .from(section.querySelector('.process_image-wrapper'), {
+        y: '3rem',
+        opacity: 0,
+        duration: 0.8,
+      });
+  });
+
+  // RESULTS SECTION
+  const projectResultsSection = document.querySelector('.section_results');
+
+  const caseResultsTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: projectResultsSection,
+      start: `top ${scrollTriggerStart}`,
+      markers: false,
+    },
+    ease: 'power3.inOut',
+  });
+
+  caseResultsTl
+    .from(projectResultsSection.querySelector('.cases_section-heading'), {
       y: '100%',
       duration: 1,
     })
     .from(
-      section.querySelector('.process_gradient'),
+      '.results_desc',
       {
-        x: '100%',
-        y: '100%',
+        y: '2rem',
         opacity: 0,
         duration: 0.8,
       },
-      0.4,
+      0.3,
     )
     .from(
-      section.querySelector('.process_stage-heading'),
-      {
-        y: '3rem',
-        opacity: 0,
-        duration: 1,
-      },
-      0.4,
-    )
-    .from(
-      section.querySelectorAll('.process-item'),
+      '.metric-item.is-case',
       {
         y: '2rem',
         opacity: 0,
         stagger: 0.2,
         duration: 0.8,
       },
-      0.8,
+      0.5,
     )
-    .from(section.querySelector('.process_image-wrapper'), {
-      y: '3rem',
-      opacity: 0,
-      duration: 0.8,
-    });
-});
-
-// RESULTS SECTION
-const projectResultsSection = document.querySelector('.section_results');
-
-const caseResultsTl = gsap.timeline({
-  scrollTrigger: {
-    trigger: projectResultsSection,
-    start: `top ${scrollTriggerStart}`,
-    markers: false,
-  },
-  ease: 'power3.inOut',
-});
-
-caseResultsTl
-  .from(projectResultsSection.querySelector('.cases_section-heading'), {
-    y: '100%',
-    duration: 1,
-  })
-  .from(
-    '.results_desc',
-    {
-      y: '2rem',
-      opacity: 0,
-      duration: 0.8,
-    },
-    0.3,
-  )
-  .from(
-    '.metric-item.is-case',
-    {
-      y: '2rem',
-      opacity: 0,
-      stagger: 0.2,
-      duration: 0.8,
-    },
-    0.5,
-  )
-  .from('.results_image', {
-    opacity: 0,
-    duration: 1.6,
-  });
-
-const imageResultsTl = gsap.timeline({
-  scrollTrigger: {
-    trigger: projectResultsSection,
-    start: 'top bottom',
-    end: 'bottom top',
-    scrub: true,
-  },
-});
-
-imageResultsTl.fromTo(
-  '.results_image',
-  {
-    y: '-3rem',
-  },
-  {
-    y: '3rem',
-  },
-);
-
-// TESTIMONIAL SECTION
-const testimonialSection = document.querySelector('.section_testimonials');
-const activeSlideMove = window.innerWidth > 991 ? '50%' : '3rem';
-
-const testimonialTl = gsap.timeline({
-  scrollTrigger: {
-    trigger: testimonialSection,
-    start: `top ${scrollTriggerStart}`,
-    markers: false,
-  },
-  ease: 'power3.inOut',
-});
-
-testimonialTl
-  .from(testimonialSection.querySelector('.section-title'), {
-    y: '100%',
-    duration: 1,
-  })
-  .from('.testimonials_slide-wrapper.is-case', {
-    y: activeSlideMove,
-    opacity: 0,
-    duration: 1.6,
-  })
-  .from(
-    testimonialSection.querySelector('.testimonials_gradient'),
-    {
-      y: '80%',
+    .from('.results_image', {
       opacity: 0,
       duration: 1.6,
+    });
+
+  const imageResultsTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: projectResultsSection,
+      start: 'top bottom',
+      end: 'bottom top',
+      scrub: true,
     },
-    '<',
-  )
-  .from('.testimonials_circle', {
-    opacity: 0,
-    duration: 0.6,
   });
 
-//FOOTER
-const footer = document.querySelector('footer');
-const footerFirstTitleLine = footer.querySelector('.cta_title--first');
-const footerSecondTitleLine = footer.querySelector('.cta_title--second');
-const footerCircles = footer.querySelectorAll('.circle-wrapper.cta-circle');
-
-const footerTl = gsap.timeline({
-  scrollTrigger: {
-    trigger: footer,
-    start: 'top 30%',
-    end: 'top 80%',
-    markers: false,
-    once: true,
-  },
-  ease: 'power3.inOut',
-});
-
-footerTl
-  .from(footer, { ease: 'linear', autoAlpha: 0 })
-  .from(footerFirstTitleLine, { y: '3rem', opacity: 0, duration: 1 })
-  .from(footerSecondTitleLine, { y: '3rem', opacity: 0, duration: 1 }, 0.3)
-  .from(
-    footerCircles,
+  imageResultsTl.fromTo(
+    '.results_image',
     {
-      x: '-50%',
-      opacity: 0,
-      stagger: { each: 0.2 },
-      duration: 1.8,
+      y: '-3rem',
     },
-    0,
-  )
-  .from('.prefooter_form-block', { y: '4rem', opacity: 0, duration: 0.8 }, 1.6)
-  .from('.prefooter_links-wrapper', { y: '1.5rem', opacity: 0, duration: 1 }, 2)
-  .from('.footer_wrapper', { y: '0.75rem', opacity: 0, duration: 0.8 }, 2.4)
-  .from('.footer_gradient', { x: '-60%', opacity: 0, duration: 1.3 }, 2.6)
-  .from('.footer_logo', { y: '5rem', opacity: 0, duration: 1.6 }, 2.8);
+    {
+      y: '3rem',
+    },
+  );
+
+  // TESTIMONIAL SECTION
+  const testimonialSection = document.querySelector('.section_testimonials');
+  const activeSlideMove = window.innerWidth > 991 ? '50%' : '3rem';
+
+  const testimonialTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: testimonialSection,
+      start: `top ${scrollTriggerStart}`,
+      markers: false,
+    },
+    ease: 'power3.inOut',
+  });
+
+  testimonialTl
+    .from(testimonialSection.querySelector('.section-title'), {
+      y: '100%',
+      duration: 1,
+    })
+    .from('.testimonials_slide-wrapper.is-case', {
+      y: activeSlideMove,
+      opacity: 0,
+      duration: 1.6,
+    })
+    .from(
+      testimonialSection.querySelector('.testimonials_gradient'),
+      {
+        y: '80%',
+        opacity: 0,
+        duration: 1.6,
+      },
+      '<',
+    )
+    .from('.testimonials_circle', {
+      opacity: 0,
+      duration: 0.6,
+    });
+
+  //FOOTER
+  const footer = document.querySelector('footer');
+  const footerFirstTitleLine = footer.querySelector('.cta_title--first');
+  const footerSecondTitleLine = footer.querySelector('.cta_title--second');
+  const footerCircles = footer.querySelectorAll('.circle-wrapper.cta-circle');
+
+  const footerTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: footer,
+      start: 'top 30%',
+      end: 'top 80%',
+      markers: false,
+      once: true,
+    },
+    ease: 'power3.inOut',
+  });
+
+  footerTl
+    .from(footer, { ease: 'linear', autoAlpha: 0 })
+    .from(footerFirstTitleLine, { y: '3rem', opacity: 0, duration: 1 })
+    .from(footerSecondTitleLine, { y: '3rem', opacity: 0, duration: 1 }, 0.3)
+    .from(
+      footerCircles,
+      {
+        x: '-50%',
+        opacity: 0,
+        stagger: { each: 0.2 },
+        duration: 1.8,
+      },
+      0,
+    )
+    .from(
+      '.prefooter_form-block',
+      { y: '4rem', opacity: 0, duration: 0.8 },
+      1.6,
+    )
+    .from(
+      '.prefooter_links-wrapper',
+      { y: '1.5rem', opacity: 0, duration: 1 },
+      2,
+    )
+    .from('.footer_wrapper', { y: '0.75rem', opacity: 0, duration: 0.8 }, 2.4)
+    .from('.footer_gradient', { x: '-60%', opacity: 0, duration: 1.3 }, 2.6)
+    .from('.footer_logo', { y: '5rem', opacity: 0, duration: 1.6 }, 2.8);
+}
