f00b545d93ac3861cd2208979f4332498fbcee43
Vitaliy Panchenko <panchenkovitalij@gmail.com>
2024-12-24 05:41:38 -0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function init() {
       { x: 2000, stagger: { each: 0.2 }, duration: 1.6 },
       1.35,
     )
-    .from('.layout352_progress-bar', { autoAlpha: 0 };
+    .from('.layout352_progress-bar', { autoAlpha: 0 });
 }
 
 window.addEventListener('load', function (event) {
